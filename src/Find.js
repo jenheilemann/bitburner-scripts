ff74a80734744aacd42bea8ffa16511ceffc6e1d
Jen Heilemann <contact@jenheilemann.com>
2021-10-28 09:53:06 -0400

    Update Find.js to be easier copy-pasting

--- src/Find.js
+++ src/Find.js
@@ -1,10 +1,34 @@
 import { networkMap } from "Network.js"
 
+const targets = {
+  "CSEC":         "CyberSec",
+  "avmnite-02h":  "NiteSec",
+  "I.I.I.I":      "The Black Hand",
+  "run4theh111z": "Bitrunners"
+}
+
 export async function main(ns) {
   let nMap = networkMap(ns).serverData
-  let path = findPath(ns.args[0], nMap)
-  ns.tprint(path)
+  if ( ns.args[0] === undefined ) {
+    for ( const server in targets ) {
+      ns.tprint("** " + server + " ( " + targets[server] + " faction)")
+      printPathToServer(ns, server, nMap, true)
+    }
+  } else {
+    printPathToServer(ns, ns.args[0], nMap)
+  }
   // path.forEach( (step) => ns.connect(step) )
+}
+
+function printPathToServer(ns, target, nMap, backdoor = false) {
+  let path = findPath(target, nMap)
+  ns.tprint(path)
+  let msg = ""
+  path.forEach((step) => msg += "connect " + step + ";")
+  if ( backdoor ) {
+    msg += "backdoor;"
+  }
+  ns.tprint(msg)
 }
 
 function findPath(goal, servers) {
