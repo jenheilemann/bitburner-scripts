1b25ab8d003b4bb8416ea351e2765edaea616078
Jen Heilemann <contact@jenheilemann.com>
2021-10-31 13:41:43 -0400

    Update buyer to wait for cash just before we purchase a server, not too early.

--- src/buyer.js
+++ src/buyer.js
@@ -1,11 +1,11 @@
 import { BestHack } from 'bestHack.js'
 import { networkMap } from 'network.js'
-import { toolsCount,waitForCash } from 'helpers.js'
+import { toolsCount, waitForCash } from 'helpers.js'
 
 const script = "get-money.script"
 
 function findTarget(ns, target, searcher) {
-  if ( target != 'dynamic' ) {
+  if (target != 'dynamic') {
     return target
   }
   return searcher.findBestPerLevel(ns, ns.getHackingLevel(), toolsCount(ns)).name
@@ -16,7 +16,7 @@ export async function main(ns) {
   ns.disableLog('sleep')
 
   const args = ns.flags([
-    ['destroy', false ],
+    ['destroy', false],
     ['target', 'dynamic'],
     ['size', 7],
   ])
@@ -24,39 +24,37 @@ export async function main(ns) {
 
   ns.tprint("Buying " + ram + "GB RAM servers")
 
-  const limit = ns.getPurchasedServerLimit();
-  const cost = ns.getPurchasedServerCost(ram);
+  const limit = ns.getPurchasedServerLimit()
+  const cost = ns.getPurchasedServerCost(ram)
   ns.tprint("Buying " + limit + " servers for " + ns.nFormat(cost, "$0.000a") + " each")
 
-  const ramRequired = ns.getScriptRam(script);
-  const threads = Math.floor(ram / ramRequired)
+  const ramRequired = ns.getScriptRam(script)
   const searcher = new BestHack(networkMap(ns).serverData)
   const pServs = ns.getPurchasedServers()
 
-  let i = 0;
-  let target, hostname;
+  let target, hostname, threads
 
-  while (i < limit) {
-    await waitForCash(ns, cost);
+  for (let i=0; i < limit; i++) {
+    hostname = "pserv-" + i
+    if (!ns.serverExists(hostname)) {
+      await waitForCash(ns, cost)
+      ns.purchaseServer(hostname, ram)
+    } else {
+      ns.scriptKill(script, hostname)
+      if (args['destroy']) {
+        ns.print("Destroying server: " + hostname)
+        ns.deleteServer(hostname)
+        await waitForCash(ns, cost)
+        ns.purchaseServer(hostname, ram)
+      }
+    }
 
     target = findTarget(ns, args.target, searcher)
     ns.print("Targeting " + target + ", ensuring sudo first.")
     ns.run("hack-server.script", 1, target, 0)
-
-    hostname = "pserv-" + i
-    if ( ! ns.serverExists(hostname) ) {
-      ns.purchaseServer(hostname, ram);
-    } else {
-      ns.scriptKill(script, hostname)
-      if ( args['destroy'] ) {
-        ns.print("Destroying server: " + hostname)
-        ns.deleteServer(hostname)
-        ns.purchaseServer(hostname, ram);
-      }
-    }
     ns.scp(script, hostname);
-    ns.exec(script, hostname, threads, target);
-    ++i;
+    threads = Math.floor(ns.getServerMaxRam(hostname) / ramRequired)
+    ns.exec(script, hostname, threads, target)
     await ns.sleep(2000)
   }
   ns.tprint("I've bought all the servers I can. It's up to you now.")
