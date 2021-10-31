f2219b68b2aa3994f36dae9f657ef2e4e3dcfbaa
Jen Heilemann <contact@jenheilemann.com>
2021-10-30 23:47:41 -0400

    Add 'reserve' to save up for the next thing we might want to buy.

--- src/helpers.js
+++ src/helpers.js
@@ -6,6 +6,15 @@ export const rootFiles = [
   "sqlinject.exe",
 ]
 
+export const darkwebCosts = {
+  "BruteSSH.exe": 500000,
+  "FTPCrack.exe": 1500000,
+  "relaySMTP.exe": 5000000,
+  "HTTPWorm.exe" : 30000000,
+  "sqlinject.exe": 250000000,
+  "Formulas.exe" : 5000000000,
+}
+
 export function toolsCount(ns) {
   let count = 0
   rootFiles.forEach((fileName) => { if (ns.fileExists(fileName)) { count++ } }, ns)
@@ -21,12 +30,21 @@ function myMoney(ns) {
 }
 
 export async function waitForCash(ns, cost) {
-  if ( myMoney(ns) >= cost ) {
+  if ( (myMoney(ns) - reserve(ns)) >= cost ) {
     ns.print("I have enough: " + ns.nFormat(cost, "$0.000a"))
     return;
   }
   ns.print("Waiting for " + ns.nFormat(cost, "$0.000a"))
-  while (myMoney(ns) < cost) {
+  while ( (myMoney(ns) - reserve(ns)) < cost) {
     await ns.sleep(3000)
   }
+}
+
+export function reserve(ns) {
+  for (let filename in darkwebCosts ) {
+    if ( !ns.fileExists(filename, 'home') ) {
+      return darkwebCosts[filename]
+    }
+  }
+  return 0
 }
