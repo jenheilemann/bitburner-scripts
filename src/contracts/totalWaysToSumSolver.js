504c3376d6de56eb1042dffe6c7672ec093589cd
Jen Heilemann <contact@jenheilemann.com>
2021-11-14 14:30:28 -0500

    Fix totalWaysToSumSolver to use correct data and submit the answer too.

--- src/contracts/totalWaysToSumSolver.js
+++ src/contracts/totalWaysToSumSolver.js
@@ -19,7 +19,12 @@ export async function main(ns) {
 
   ns.tprint(`Found ${args.file} (${args.type}) on ${args.server}`)
   let answer = solve(data)
-  let result = ns.codingcontract.attempt('', args.file, args.server, { returnReward: true })
+  let result = ns.codingcontract.attempt(
+    answer,
+    args.file,
+    args.server,
+    { returnReward: true }
+  )
   ns.tprint(`${args.file} attempt result: ${result}`)
 }
 
