61185633c0b6aa80e6af11927392ecf7eb84cf36
Jen Heilemann <contact@jenheilemann.com>
2021-11-17 16:04:33 -0500

    Fix a typo I gained in all the copy-pasting.

--- src/formulae/hacking.js
+++ src/formulae/hacking.js
@@ -1,5 +1,5 @@
 
-export function calculateIntelligenceBonus(intelligence weight = 1) {
+export function calculateIntelligenceBonus(intelligence, weight = 1) {
   return 1 + (weight * Math.pow(intelligence, 0.8)) / 600;
 }
 
