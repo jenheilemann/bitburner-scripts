0813046da62b6188d0a5018a939176668c403dc3
crazykid080 <10173541+crazykid080@users.noreply.github.com>
2021-11-18 14:42:17 -0700

    Fix missing thread argument

--- src/initStartup.js
+++ src/initStartup.js
@@ -71,7 +71,7 @@ export async function main(ns) {
 
   await ns.sleep(50)
   ns.tprint(`Starting startup/run.js`)
-  ns.spawn('/startup/run.js')
+  ns.spawn('/startup/run.js', 1)
 }
 
 export async function download(ns, filename) {
