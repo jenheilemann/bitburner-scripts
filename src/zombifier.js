2b62dd991d4a0dcc4dbacda1114bda8c409ea4c2
Jen Heilemann <contact@jenheilemann.com>
2021-11-24 23:07:10 -0500

    Cheapen up the zombifier.

--- src/zombifier.js
+++ src/zombifier.js
@@ -1,4 +1,5 @@
 import { fetchServerFree } from 'network.js'
+import { getNsDataThroughFile as fetch } from 'helpers.js'
 const script = 'breadwinner.js'
 
 export function autocomplete(data, args) {
@@ -13,23 +14,23 @@ export async function main(ns) {
   var toHack = ns.args[1]
 
   // copy the scripts to the target
-  await ns.scp(script, "home", target.name);
-  ns.print("Copied " + script + " to " + target.name)
+  await fetch(ns, `ns.scp(${script}, "home", ${target.name})`)
+  ns.print(`Copied ${script} to ${target.name}`)
 
   // calculate the threads we can use for running our script
-  var ramRequired = ns.getScriptRam(script);
+  var ramRequired = await fetch(ns, `ns.getScriptRam(${script})`)
   var availableRam = target.maxRam - target.data.ramUsed
-  ns.print(target.name + " has " + availableRam + " ram available to use")
-  var threads = Math.min(50, Math.floor(availableRam / ramRequired))
+  ns.print(`${target.name} has ${availableRam} ram available to use`)
+  var threads = Math.floor(availableRam / ramRequired)
 
   if (threads < 1) {
-    ns.print("No ram available")
-    ns.exit()
-    return;
+    ns.tprint(`No ram available to run ${script} on ${target.name} ` +
+      `(${availableRam}/${ramRequired})`)
+    return
   }
 
   ns.print("Using " + threads + " threads")
 
-  var pid = ns.exec(script, target.name, threads, toHack, threads);
-  ns.print("Running script on " + target.name + " with PID " + pid)
+  var pid = await fetch(ns, `ns.exec('${script}', '${target.name}', ${threads}, '${toHack}', ${threads})` )
+  ns.print(`Running script on ${target.name} with PID ${pid}`)
 }
