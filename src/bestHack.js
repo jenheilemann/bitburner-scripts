12459f965ea6219741be735795f1c9a0952088d0
Jen Heilemann <contact@jenheilemann.com>
2021-11-24 23:06:16 -0500

    Make bestHack.js work correctly and be a bit cheaper.

--- src/bestHack.js
+++ src/bestHack.js
@@ -1,5 +1,5 @@
-import { toolsCount, fetchPlayer } from 'helpers.js'
-import { networkMap } from 'network.js'
+import { fetchPlayer } from 'helpers.js'
+import { networkMapFree } from 'network.js'
 import { calculateWeakenTime } from '/formulae/hacking.js'
 
 const maxMoneyCoefficient = 1.25
@@ -14,9 +14,9 @@ export function calcScore(server) {
   let money = Math.pow(server.maxMoney, maxMoneyCoefficient)
   let growth = Math.pow(Math.min(server.growth, growthCap), growthCoefficient)
   let minSec = Math.pow(server.minSecurity, minSecurityCoefficient)
-  let hack = server.hackingLvl
+  let hacking = server.hackingLvl
 
-  return (money * growth / (securityWeight + minSec * hack))
+  return (money * growth / (securityWeight + minSec * hacking))
 }
 
 export class BestHack {
@@ -28,7 +28,7 @@ export class BestHack {
     let scores = this.calcServerScores()
     let filtered = Object.values(scores)
       .filter((server) => server.hackingLvl <= player.hacking &&
-                          server.hasAdminRights &&
+                          server.data.hasAdminRights &&
                           calculateWeakenTime(server.data, player) < maxWeakenTime)
     return filtered.reduce((prev, current) => (prev.score > current.score) ? prev : current)
   }
@@ -46,7 +46,6 @@ export class BestHack {
 }
 
 export async function main(ns) {
-  let searcher = new BestHack(await networkMap(ns))
-  let player = fetchPlayer()
-  ns.tprint(searcher.findBestPerLevel(player) )
+  let searcher = new BestHack(await networkMapFree())
+  ns.tprint( searcher.findBestPerLevel(fetchPlayer()) )
 }
