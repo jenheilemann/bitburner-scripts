e10536f2acbf73d06338a79e95d8e73a64690c97
Jen Heilemann <contact@jenheilemann.com>
2021-11-26 15:10:49 -0500

    Add a new stats script for adding stats to the HUD.

---
+++ src/stats.js
@@ -0,0 +1,28 @@
+import { getNsDataThroughFile as fetch } from './helpers.js'
+
+/** @param {NS} ns **/
+export async function main(ns) {
+  const doc = eval('document')
+  const hook0 = doc.getElementById('overview-extra-hook-0')
+  const hook1 = doc.getElementById('overview-extra-hook-1')
+  // hook0.innerText = "Hashes\nStock\nScrInc\nScrExp"
+  hook0.innerText = "ScrInc\nScrExp"
+
+  while (true) {
+    // const hashes = await getNsDataThroughFile(ns, '[ns.hacknet.numHashes(), ns.hacknet.hashCapacity()]', '/Temp/hash-stats.txt')
+    // const stkSymbols = await getNsDataThroughFile(ns, `ns.stock.getSymbols()`, '/Temp/stock-symbols.txt');
+    // const stkPortfolio = await getNsDataThroughFile(ns, JSON.stringify(stkSymbols) +
+    //   `.map(sym => ({ sym, pos: ns.stock.getPosition(sym), ask: ns.stock.getAskPrice(sym), bid: ns.stock.getBidPrice(sym) }))` +
+    //   `.reduce((total, stk) => total + stk.pos[0] * stk.bid + stk.pos[2] * (stk.ask * 2 - stk.bid), 0)`,
+    //   '/Temp/stock-portfolio-value.txt')
+    // hook1.innerText =
+    //   `${formatNumberShort(hashes[0], 3, 0)}/${ns.nFormat(hashes[1], 3, 0)}` +
+    //   `\n${formatMoney(stkPortfolio)}` +
+    //   `\n${ns.nFormat(ns.getScriptIncome()[0], '$0,0')}/s` +
+    //   `\n${ns.nFormat(ns.getScriptExpGain(), 3, 2)}/s`
+    hook1.innerText =
+      `${ns.nFormat(ns.getScriptIncome()[0], "$0.00a")}/s` +
+      `\n${ns.nFormat(ns.getScriptExpGain(), "0,0.00")}/s`
+    await ns.sleep(1000)
+  }
+}
