40a687483b87ac3200c63ce283a289f10039dfe0
Jen Heilemann <jenmwhiting@gmail.com>
2021-12-20 10:32:10 -0500

    Add the ability to specify the branch when running startup.

--- README.md
+++ README.md
@@ -31,11 +31,13 @@ export async function main(ns) {
     throw new Exception("Run the script from home");
   }
 
+  let args = ns.flags([['branch', 'main']])
+
   await ns.wget(
-    `https://raw.githubusercontent.com/jenheilemann/bitburner-scripts/main/src/initStartup.js?ts=${new Date().getTime()}`,
+    `https://raw.githubusercontent.com/jenheilemann/bitburner-scripts/${args.branch}/src/initStartup.js?ts=${new Date().getTime()}`,
     "initStartup.js"
   );
-  ns.spawn("initStartup.js", 1);
+  ns.spawn("initStartup.js", 1, '--branch', args.branch);
 }
 ```
 
--- src/initStartup.js
+++ src/initStartup.js
@@ -60,19 +60,21 @@ const filesToDownload = [
   'weaken.js',
   'workForFactions.js',
 ]
-const baseUrl = 'https://raw.githubusercontent.com/jenheilemann/bitburner-scripts/main/src'
+const baseUrl = 'https://raw.githubusercontent.com/jenheilemann/bitburner-scripts/'
+
 
 /**
  * @param {NS} ns
  **/
 export async function main(ns) {
   ns.disableLog("sleep")
+  const args = ns.flags([['branch', 'main']])
 
   for ( let filename of filesToDownload ) {
     ns.scriptKill(filename, 'home')
     ns.rm(filename)
     await ns.sleep(50)
-    await download(ns, filename)
+    await download(ns, filename, args.branch)
   }
   await ns.sleep(50)
   ns.tprint('Killed and deleted old scripts.')
@@ -84,9 +86,9 @@ export async function main(ns) {
   ns.spawn('/startup/run.js', 1)
 }
 
-export async function download(ns, filename) {
+export async function download(ns, filename, branch) {
   const fileUrl = filename.includes("/") ? filename : "/" + filename;
-  const path = baseUrl + fileUrl
+  const path = baseUrl + branch + '/src' + fileUrl
   ns.tprint(`Trying to download ${path}`)
   await ns.wget(path + '?ts=' + new Date().getTime(), filename)
 }
