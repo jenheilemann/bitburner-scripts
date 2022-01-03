e9257dcfc6e8c7471f5dda32a2591ea9f912dd28
Jen Heilemann <jenmwhiting@gmail.com>
2022-01-03 12:00:00 -0500

    Make sleeve int farming a bit more intelligent.

--- src/sleeves/manager.js
+++ src/sleeves/manager.js
@@ -2,6 +2,7 @@ import {
   getNsDataThroughFile as fetch,
   getLSItem,
   haveSourceFile,
+  fetchPlayer,
 } from 'helpers.js'
 
 /** @param {NS} ns **/
@@ -14,6 +15,7 @@ export async function main(ns) {
     return ns.print('no sleeves yet, wyd?')
 
   for ( const sleeve of sleeveData ) {
+    ns.print(`Managing activity for sleeve #${sleeve.number}:`)
     await manageSleeve(ns, sleeve)
   }
 }
@@ -23,16 +25,27 @@ export async function main(ns) {
  * @param {object} sleeve
  **/
 async function manageSleeve(ns, sleeve) {
-  if ( sleeve.stats.sync < 95 )
+  if ( sleeve.stats.sync < 95 ) {
+    ns.print(`Sleeve sync at ${formatNumberShort(sleeve.stats.sync)}, syncronizing....`)
     return await syncronize(ns, sleeve)
+  }
 
   const gangMeta = getLSItem('gangMeta')
-  if ( !gangMeta || !gangMeta.faction )
+  if ( !gangMeta || !gangMeta.faction ) {
+    ns.print(`Not yet joined a gang, reducing karma`)
     return await assistJoiningGang(ns, sleeve)
+  }
 
   // if ( sleeve.stats.shock > 0 )
   //   return await goToTherapy(ns, sleeve)
 
+  const player = fetchPlayer()
+  if ( ['strength', 'defense', 'dexterity', 'agility'].some(s => player[s] < 200 ) ) {
+    ns.print(`some stat is less than 200, mugging.`)
+    return await farmCombatStats(ns, sleeve)
+  }
+
+  ns.print(`farming int`)
   return await farmIntelligence(ns, sleeve)
 }
 
@@ -41,10 +54,40 @@ async function manageSleeve(ns, sleeve) {
  * @param {object} sleeve
  **/
 async function farmIntelligence(ns, sleeve) {
-  if ( sleeve.task.crime == "Larceny" )
-    return
+  if ( bondForgeryChance(sleeve) > 0.75 ) {
+    if ( sleeve.task.crime == 'Bond Forgery' )
+      return ns.print(`Sleeve #${sleeve.number} already forging bonds.`)
+    await fetch(ns, `ns.sleeve.setToCommitCrime(${sleeve.number}, "Bond Forgery")`,
+      '/Temp/sleeve.setToCommitCrime.txt')
+    return ns.print(`Sleeve #${sleeve.number} set to forging bonds.`)
+  }
 
-  await fetch(ns, `ns.sleeve.setToCommitCrime(${sleeve.number}, "Larceny")`,
+  if ( larcenyChance(sleeve) > 0.75 ) {
+    if ( sleeve.task.crime == 'Larceny' )
+      return ns.print(`Sleeve #${sleeve.number} already committing larceny.`)
+    await fetch(ns, `ns.sleeve.setToCommitCrime(${sleeve.number}, "Larceny")`,
+      '/Temp/sleeve.setToCommitCrime.txt')
+    return ns.print(`Sleeve #${sleeve.number} set to larceny.`)
+  }
+
+  if ( sleeve.task.crime == "Rob Store" )
+    return ns.print(`Sleeve #${sleeve.number} already robbing store.`)
+
+  ns.print(`set to commit crime rob store`)
+  await fetch(ns, `ns.sleeve.setToCommitCrime(${sleeve.number}, "Rob Store")`,
+    '/Temp/sleeve.setToCommitCrime.txt')
+}
+
+/**
+ * @param {NS} ns
+ * @param {object} sleeve
+ **/
+async function farmCombatStats(ns, sleeve) {
+  if ( sleeve.task.crime == "Mug" )
+    return ns.print('already mugging')
+
+  ns.print(`set to commit crime mug`)
+  await fetch(ns, `ns.sleeve.setToCommitCrime(${sleeve.number}, "Mug")`,
     '/Temp/sleeve.setToCommitCrime.txt')
 }
 
@@ -92,8 +135,31 @@ async function syncronize(ns, sleeve) {
 }
 
 function homicideChance(sleeve) {
+  const playerInt = fetchPlayer().intelligence
+  const difficulty = 1
   return (((2*sleeve.stats.strength +
             2*sleeve.stats.defense +
             0.5*sleeve.stats.dexterity +
-            0.5*sleeve.stats.agility ) / 975 ) / 1 )
+            0.5*sleeve.stats.agility +
+            0.25*playerInt) / 975 ) / difficulty ) *
+            (1+((1*(playerInt^0.8))/600))
+}
+
+function bondForgeryChance(sleeve) {
+  const playerInt = fetchPlayer().intelligence
+  const difficulty = 0.5
+
+  return (((0.05*sleeve.stats.hacking +
+    1.25*sleeve.stats.dexterity) / 975) / difficulty )*
+    (1 + ((1*(playerInt^0.8))/600))
+}
+
+function larcenyChance(sleeve) {
+  const playerInt = fetchPlayer().intelligence
+  const difficulty = 0.33
+
+  return (((0.5*sleeve.stats.hacking +
+    1*sleeve.stats.dexterity +
+    1*sleeve.stats.agility) / 975) / difficulty )*
+    (1 + ((1*(playerInt^0.8))/600))
 }
