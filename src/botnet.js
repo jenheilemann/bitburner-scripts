a773a5a484e06721a33ce0d650c52542384db050
Jen Heilemann <jenmwhiting@gmail.com>
2025-04-11 10:07:00 -0400

    Add option to botnet to allow overwriting files.

--- src/botnet.js
+++ src/botnet.js
@@ -6,22 +6,29 @@ import {
 // magic number (Ram required to run hack.js)
 const hackingScriptSize = 1.7
 const scripts = ['breadwinner.js', 'batchGrow.js', 'batchHack.js', 'batchWeaken.js']
+const argsSchema = [['overwrite', false]]
+
+export function autocomplete(data, args) {
+  data.flags(argsSchema)
+}
 
 /**
  * @param {NS} ns
  **/
 export async function main(ns) {
   disableLogs(ns, ['sleep', 'scp'])
-  let servers = fetchZombifyableServers(ns)
+  let options = ns.flags(argsSchema)
+  ns.print(options.overwrite)
+  let servers = fetchZombifyableServers(ns, options.overwrite)
   if (!servers) return
 
   ns.tprint("Zombifying " + servers.length + " servers")
   var success = []
   var failure = []
   for (let server of servers) {
-    var res = await zombify(ns, server.name)
+    var res = zombify(ns, server.name)
     res ? success.push(server) : failure.push(server)
-    await ns.sleep(50)
+    await ns.sleep(20)
   }
   if (success.length > 0 ) {
     let msg = `Zombified servers: ${success.map(s => s.name).join(', ')}`
@@ -35,7 +42,6 @@ export async function main(ns) {
   }
 }
 
-
 /**
  * @param {array} files - files on a server
  * @returns {boolean} - whether those files include all the scripts we need to upload
@@ -44,14 +50,14 @@ function hasAllScripts(files) {
   return scripts.every((script) => files.includes(script))
 }
 
-
 /**
  * fetchZombifyableServers
  * Find any servers in the network that haven't gotten the copied files yet.
  * @param {NS} NS
+ * @param {boolean} overwrite - overwrite existing filess
  * @returns {boolean|array} - servers we can work on, or false if none available
  **/
-function fetchZombifyableServers(ns) {
+function fetchZombifyableServers(ns, overwrite) {
   ns.print("Fetching servers from nmap")
   let nmap = getLSItem('nmap')
   if (! nmap ) {
@@ -62,7 +68,7 @@ function fetchZombifyableServers(ns) {
     .filter(s => s.hasAdminRights &&
                 s.name != 'home' &&
                 s.maxRam >= hackingScriptSize &&
-                !hasAllScripts(ns.ls(s.hostname))
+                (overwrite || !hasAllScripts(ns.ls(s.hostname)))
     )
   ns.print(servers.map(s => [s.name, s.hasAdminRights, s.maxRam, s.ramUsed, s.files]))
   // early return, if there are no servers no need to do anything else
@@ -78,7 +84,7 @@ function fetchZombifyableServers(ns) {
  * @param {string} server - Server host name
  * @returns {boolean} - whether the files successfully copied
  **/
-async function zombify(ns, server) {
+function zombify(ns, server) {
   var res = ns.scp(scripts, server, "home")
   if ( res ) {
     ns.print(`Copied ${scripts} to ${server}`)
